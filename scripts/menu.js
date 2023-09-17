298ed03621b96b81e28c291a806d59acd2cd2388
Sharqy <pencilmagicart@gmail.com>
2023-09-17 11:41:21 +0300

    Menu Animation

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,8 +2,8 @@ let burgerIcon = document.querySelector("#burger");
 let menu = document.querySelector("#menu")
 
 burgerIcon.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-    if (menu.classList.contains("hidden"))
+    menu.classList.toggle("active");
+    if (!menu.classList.contains("active"))
         burgerIcon.src = "./assets/shared/icon-hamburger.svg";
     else
         burgerIcon.src = "./assets/shared/icon-close.svg";
