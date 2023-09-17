0d230161fdd1e7ad420269622d0880032e6f87bb
Sharqy <pencilmagicart@gmail.com>
2023-09-17 11:34:32 +0300

    Finished tech.js

--- scripts/crew.js
+++ scripts/crew.js
@@ -1,19 +1,22 @@
-let members = document.querySelectorAll(".crewMem");
-let crewImg = document.querySelector("#crewImg");
+let imageBox;
+let crewImg;
+let content = document.querySelector(".content");
 let crewRole = document.querySelector("#crewRole");
 let crewName = document.querySelector("#crewName");
 let crewDesc = document.querySelector("#crewDesc");
+
+let crewSelectorGroup = document.querySelector("#crewSelectorGroup");
 
 getMembers();
 
 async function getMembers() {
     let response = await fetch("./data.json");
     let data = (await response.json()).crew;
-    console.log(data);
-    displayCrewInfo(data);
+    createInfoHolders(data)
 }
 
-function displayCrewInfo(array) {
+function selectCrewMem(array) {
+    members = document.querySelectorAll(".crewMem");
     members.forEach(member => {
         member.addEventListener("click", () => {
             if (!member.classList.contains("active")) {
@@ -21,11 +24,61 @@ function displayCrewInfo(array) {
                 cur.classList.remove("active");
                 member.classList.add("active");
             }
+            let curActive = document.querySelector(`.crewImageBox[data-status="active"]`);
+            let curIndex = curActive.dataset.index;
+            let newActive = document.querySelector(`.crewImageBox[data-index="${member.dataset.index}"]`)
+            if (curIndex != member.dataset.index) {
+                if (member.dataset.index > curIndex) {
+                    newActive.dataset.status = "nextRight";
+                    curActive.dataset.status = "nextLeft";
+                }
+                else if (member.dataset.index < curIndex) {
+                    newActive.dataset.status = "nextLeft";
+                    curActive.dataset.status = "nextRight";
+                }
+                setTimeout(() => {
+                    newActive.dataset.status = "active";
+                    curActive.dataset.status = "pending";
+                    curIndex = member.dataset.index;
+                }, 300);
+            }
             let memberIndex = member.dataset.index
-            crewImg.src = array[memberIndex].images.png;
             crewName.innerHTML = array[memberIndex].name.toUpperCase();
             crewRole.innerHTML = array[memberIndex].role.toUpperCase();
             crewDesc.innerHTML = array[memberIndex].bio;
         })
     })
+}
+
+function createInfoHolders(array) {
+    // Create crewMem selector for each crewMem in JSON file
+    for (let i = 0; i < array.length; i++) {
+        let crewMem = document.createElement("div");
+        crewMem.classList.add("crewMem", "w-3", "aspect-square", "bg-greyObj", "rounded-full", "cursor-pointer");
+        if (i == 0)
+            crewMem.classList.add("active");
+        crewMem.dataset.index = i;
+        crewSelectorGroup.appendChild(crewMem);
+    }
+    let members = document.querySelectorAll(".crewMem");
+
+    // Create crewMem image for each crewMem in JSON file
+    members.forEach(member => {
+        imageBox = document.createElement("div");
+        imageBox.classList.add("crewImageBox", "absolute", "border-b-2", "w-[350px]", "border-greyLine", "flex", "justify-center", "sm:w-[650px]", "sm:border-0", "sm:bottom-0", "left-1/2", "-translate-x-1/2", "lg:w-1/2", "lg:left-3/4")
+        imageBox.dataset.index = member.dataset.index;
+        imageBox.dataset.status = "pending";
+        crewImg = document.createElement("img");
+        crewImg.src = array[+member.dataset.index].images.png;
+        crewImg.classList.add("crewImg", "w-1/2", "lg:w-1/2", "duration-300");
+        if (imageBox.dataset.index == 0) {
+            imageBox.dataset.status = "active";
+        }
+        imageBox.appendChild(crewImg);
+        content.prepend(imageBox);
+    })
+    crewName.innerHTML = array[0].name.toUpperCase();
+    crewRole.innerHTML = array[0].role.toUpperCase();
+    crewDesc.innerHTML = array[0].bio;
+    selectCrewMem(array);
 }
--- scripts/tech.js
+++ scripts/tech.js
@@ -1,39 +1,74 @@
-let techies = document.querySelectorAll(".tech");
-let techImg = document.querySelector("#techImg");
+let imageBox;
+let techImg;
 let techName = document.querySelector("#techName");
 let techDesc = document.querySelector("#techDesc");
+let content = document.querySelector(".content")
+
+let techSelectorGroup = document.querySelector("#techSelectorGroup")
 
 getTechies();
 //"./assets/technology/image-launch-vehicle-landscape.jpg"
 async function getTechies() {
     let response = await fetch("./data.json");
     let data = (await response.json()).technology;
-    console.log(data);
-    if (window.matchMedia("(min-width: 1024px)").matches) {
-        techImg.src = data[0].images.portrait;
-    } else {
-        techImg.src = data[0].images.landscape;
-    }
-    displayTechInfo(data);
+    createInfoHolders(data);
 
-    function displayTechInfo(array) {
-        techies.forEach(techie => {
-            techie.addEventListener("click", () => {
-                if (!techie.classList.contains("active")) {
-                    let cur = document.querySelector(".tech.active");
-                    cur.classList.remove("active");
-                    techie.classList.add("active");
-                }
-                let techieIndex = techie.dataset.index
-                if (window.matchMedia("(min-width: 1024px)").matches) {
-                    techImg.src = array[techieIndex].images.portrait;
-                } else {
-                    techImg.src = array[techieIndex].images.landscape;
-                }
+}
 
-                techName.innerHTML = array[techieIndex].name.toUpperCase();
-                techDesc.innerHTML = array[techieIndex].description;
-            })
+function selectTech(array) {
+    techies = document.querySelectorAll(".tech");
+    techies.forEach(techie => {
+        techie.addEventListener("click", () => {
+            if (!techie.classList.contains("active")) {
+                let cur = document.querySelector(".tech.active");
+                cur.classList.remove("active");
+                techie.classList.add("active");
+            }
+            let techieIndex = techie.dataset.index
+            let curActive = document.querySelector(`.techImageBox[data-status="active"]`);
+            let newActive = document.querySelector(`.techImageBox[data-index="${techieIndex}"]`)
+            curActive.dataset.status = "pending";
+            newActive.dataset.status = "active";
+            techName.innerHTML = array[techieIndex].name.toUpperCase();
+            techDesc.innerHTML = array[techieIndex].description;
         })
+    })
+}
+
+function createInfoHolders(array) {
+    // Create tech selector for each tech in JSON file
+    for (let i = 0; i < array.length; i++) {
+        let techie = document.createElement("div");
+        techie.classList.add("tech", "w-10", "aspect-square", "rounded-full", "border", "border-white", "grid", "place-items-center", "cursor-pointer", "sm:w-16");
+        if (i == 0)
+            techie.classList.add("active");
+        techie.dataset.index = i;
+        techie.innerHTML = i + 1;
+        techSelectorGroup.appendChild(techie);
     }
+    let techies = document.querySelectorAll(".tech");
+
+    // Create tech image for each tech in JSON file
+    techies.forEach(techie => {
+        imageBox = document.createElement("div");
+        imageBox.classList.add("techImageBox", "absolute", "w-full", "mb-6", "sm:mb-10", "lg:w-1/4", "lg:absolute", "lg:right-0", "lg:top-1/2", "lg:-translate-y-1/2");
+        imageBox.dataset.index = techie.dataset.index;
+        imageBox.dataset.status = "pending";
+        techImg = document.createElement("img");
+        techImg.src = array[+techie.dataset.index].images.png;
+        techImg.classList.add("techImg", "w-full");
+        if (imageBox.dataset.index == 0) {
+            imageBox.dataset.status = "active";
+        }
+        if (window.matchMedia("(min-width: 1024px)").matches) {
+            techImg.src = array[imageBox.dataset.index].images.portrait;
+        } else {
+            techImg.src = array[imageBox.dataset.index].images.landscape;
+        }
+        imageBox.appendChild(techImg);
+        content.prepend(imageBox);
+    })
+    techName.innerHTML = array[0].name.toUpperCase();
+    techDesc.innerHTML = array[0].description;
+    selectTech(array);
 }
